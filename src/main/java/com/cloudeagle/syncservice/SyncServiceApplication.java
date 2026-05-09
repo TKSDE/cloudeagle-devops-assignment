52eef5d0d2117363be6ce8f0bbe9025a3b44974b
tksde <TKSDE@users.noreply.github.com>
2026-05-09 14:52:43 +0530

    Fix URI in application.yaml and add Welcome endpoint

--- src/main/java/com/cloudeagle/syncservice/SyncServiceApplication.java
+++ src/main/java/com/cloudeagle/syncservice/SyncServiceApplication.java
@@ -2,12 +2,20 @@ package com.cloudeagle.syncservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SyncServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SyncServiceApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to CloudEagle Sync Service API! The application is running successfully.";
     }
 
 }
